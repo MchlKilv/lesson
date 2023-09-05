74b7577d396cfc207219cd5a0c4f2240e5cd83db
Mike <mchlkiselev@gmail.com>
2023-09-05 22:31:30 +0300

    less#4

---
+++ less#4.py
@@ -0,0 +1,22 @@
+# def f(x):
+#     return x * x
+
+
+# a = f
+# print(a(5))
+# print(f(5) # передача ссылкой
+
+
+# def calk1(a, b):  # a, b - несколько перем
+#     return a + b
+
+
+# def calk2(a, b):
+#     return a * b
+
+
+# def math(op, x, y):
+#     print(op(x, y))
+
+
+# math(calk1, 5, 45)  # calk1, calk2... передаем по ссылке в ор
